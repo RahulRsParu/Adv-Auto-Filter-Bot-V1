4c7a51a5cebf03523f18214a3c51cd40de973285
RahulRsParu <88199060+RahulRsParu@users.noreply.github.com>
2021-08-08 14:15:48 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    ' 🙏please join our channel🙏 ', url="https://t.me/movie_center_RU"
                                 )
                         ]
                     ]
